741b50f95b24856ef78805aa574027f06e7023a1
Dr.klo <leonid.kulinchik@gotechsoftware.com>
2018-02-19 14:14:09 +0300

    fix to publish

--- src/numeric-mask.module.ts
+++ src/numeric-mask.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { NumericMaskDirective} from './numeric-mask.directive';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     NumericMaskDirective
+  ], exports: [
+    NumericMaskDirective
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: []
+    CommonModule,
+    FormsModule
+  ]
 })
 export class NumericMaskModule { }
