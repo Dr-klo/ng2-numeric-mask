16849d95854b99c9aa47b22f4e88ee97aef505a3
Dr.klo <leonid.kulinchik@gotechsoftware.com>
2018-02-19 14:11:41 +0300

    fix to publish

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import {NumericMaskModule} from './app/numeric-mask.module';
-
-enableProdMode();
-platformBrowserDynamic().bootstrapModule(NumericMaskModule)
-  .catch(err => console.log(err));
